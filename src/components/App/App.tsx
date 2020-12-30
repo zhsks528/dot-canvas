cef26a335335f5c41c84172b37fb4e0753843d87
SeongMin Han <hsm950825@naver.com>
2020-12-30 20:00:02 +0900

    [Add] Canvas resize Method

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
 
+let canvas: HTMLCanvasElement;
+let ctx: CanvasRenderingContext2D | null;
+
 const App = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const pixelRatio: number = window.devicePixelRatio > 1 ? 2 : 1;
+
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+
+    canvas = canvasRef.current;
+    ctx = canvasRef.current.getContext("2d");
+
+    window.addEventListener("resize", resize, false);
+
+    resize();
+  }, []);
+
+  const resize = () => {
+    canvas.width = document.body.clientWidth * pixelRatio;
+    canvas.height = document.body.clientHeight * pixelRatio;
+
+    // 캔버스 확대 요소
+    ctx!.scale(pixelRatio, pixelRatio);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <canvas ref={canvasRef}></canvas>
+    </>
   );
 };
 
