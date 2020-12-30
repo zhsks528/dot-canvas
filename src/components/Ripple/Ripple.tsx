ff20ed2dd7fc5d1dafba2409ad2612905512ed6e
SeongMin Han <hsm950825@naver.com>
2020-12-30 20:14:36 +0900

    [Add] Ripple Class

---
+++ src/components/Ripple/Ripple.tsx
@@ -0,0 +1,17 @@
+export class Ripple {
+  public x: number;
+  public y: number;
+  public radius: number;
+  public maxRadius: number;
+  public speed: number;
+  public stageWidth: any;
+  public stageHeight: any;
+
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.radius = 0;
+    this.maxRadius = 0;
+    this.speed = 20;
+  }
+}
