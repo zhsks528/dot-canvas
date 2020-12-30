ff642e7f2982d736d92ba1f58c675314d0e140c3
SeongMin Han <hsm950825@naver.com>
2020-12-30 20:30:27 +0900

    [Add] Functions(animate, reset) within Dot Class

--- src/components/Dot/dot.tsx
+++ src/components/Dot/dot.tsx
@@ -1,3 +1,6 @@
+const PI2 = Math.PI * 2;
+const BOUNCE = 0.82;
+
 export class Dot {
   public targetRadius: number;
   public radiusV: number;
@@ -23,4 +26,30 @@ export class Dot {
     this.green = green;
     this.blue = blue;
   }
+
+  animate = (ctx: any): void => {
+    ctx.beginPath();
+    ctx.fillStyle = "#000";
+    ctx.fillRect(
+      this.x - this.pixelSizeHalf,
+      this.y - this.pixelSizeHalf,
+      this.pixelSize,
+      this.pixelSize
+    );
+
+    const accel = (this.targetRadius - this.radius) / 2;
+    this.radiusV += accel;
+    this.radiusV *= BOUNCE;
+    this.radius += this.radiusV;
+
+    ctx.beginPath();
+    ctx.fillStyle = `rgb(${this.red}, ${this.green}, ${this.blue})`;
+    ctx.arc(this.x, this.y, this.radius, 0, PI2, false);
+    ctx.fill();
+  };
+
+  reset = () => {
+    this.radius = 0;
+    this.radiusV = 0;
+  };
 }
